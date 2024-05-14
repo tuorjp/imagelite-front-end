2e7e9f53b5b5aa5a9dd04740cf02d6a9dac5f123
JP <jplmarotinho22@gmail.com>
2024-05-14 18:07:36 -0300

    api call added, ImageCards render

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,28 +1,39 @@
 import { Template } from '../components/Template'
 import { ImageCard } from '../components/ImageCard'
+import { useImageService } from '../resources/image/image.service'
+import { Image } from '../resources/image/image.resource'
+import { useState } from 'react';
 
 export default function GaleriaPage() {
+    const [images, setImages] = useState<Image[]>([]);
+    const useService = useImageService();
+    
+    async function searchImages() {
+        const imagesResponse = await useService.buscar();
+        setImages(imagesResponse);
+        console.table(imagesResponse);
+    }
+
+    function renderImageCard(img: Image, i: number) {
+        return (
+            <ImageCard 
+                nome={img.name} 
+                src={img.url} 
+                tamanho={img.size} 
+                dataUpload={img.uploadDate}
+                key={i}
+            />
+        )
+    }
+
+    function renderImageCards() {
+        return images.map((img, i) => renderImageCard(img, i));
+    }
+
     return (
         <Template>
             <section className='grid grid-cols-3 gap-8'>
-                <ImageCard 
-                    src='https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    nome='exemplo' 
-                    tamanho='10MB' 
-                    dataUpload='01/01/2024'
-                />
-                <ImageCard 
-                    src='https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    nome='exemplo' 
-                    tamanho='10MB' 
-                    dataUpload='01/01/2024'
-                />
-                <ImageCard 
-                    src='https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                    nome='exemplo' 
-                    tamanho='10MB' 
-                    dataUpload='01/01/2024'
-                />
+                {renderImageCards()}
             </section>
         </Template>
     )
