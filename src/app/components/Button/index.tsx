33c0fd05138b6882f6f9d6929d93d0c00c32cb90
JP <jplmarotinho22@gmail.com>
2024-05-16 16:21:24 -0300

    formik and yup validação

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -3,13 +3,15 @@ interface ButtonProps {
     label?: string;
     hover?: string;
     onClick?: (event: any) => void;
+    type?: "submit" | "reset" | "button" | undefined;
 }
 
-export const Button: React.FC<ButtonProps> = ({onClick, color, label, hover}) => {
+export const Button: React.FC<ButtonProps> = ({onClick, color, label, hover, type}) => {
     return (
         <button 
             className={`${color} text-white px-4 py-2 rounded-md hover:${hover}`} 
             onClick={onClick}
+            type={type}
         >
             {label}
         </button>
--- src/app/components/InputText/index.tsx
+++ src/app/components/InputText/index.tsx
@@ -2,15 +2,17 @@ interface InputTextProps {
     style?: string;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     placeholder?: string;
+    id?: string
 }
 
-export const InputText: React.FC<InputTextProps> = ({ onChange, style, placeholder }) => {
+export const InputText: React.FC<InputTextProps> = ({ onChange, style, placeholder, id }) => {
     return (
         <input 
             type="text" 
             className={`${style} border px-3 py-2 rounded-md text-gray-900`}
             onChange={onChange}
             placeholder={placeholder}
+            id={id}
         />
     )
 }
