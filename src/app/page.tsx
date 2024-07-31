4cf5d4511e3d7d27150979488f3a735276b85b6e
JP <jplmarotinho22@gmail.com>
2024-07-31 16:56:59 -0300

    LoginPage, GaleriaPage dynamic render depending on user session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
-  return (
-    <>
+'use client'
 
-    </>
+import { useAuth } from "./resources"
+import LoginPage from "./login/page"
+import GaleriaPage from "./galeria/page"
+
+export default function Home() {
+  const auth = useAuth()
+  const user = auth.getUserSession()
+
+  if(!user) {
+    return <LoginPage />
+  }
+
+  return (
+    <GaleriaPage />
   )
 }
